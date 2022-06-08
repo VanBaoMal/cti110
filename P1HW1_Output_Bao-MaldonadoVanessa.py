fe1dfc83581ee1f87ed5e75c7a98376142af36b4
VanBaoMal <107140213+VanBaoMal@users.noreply.github.com>
2022-06-08 13:46:51 -0400

    Add files via upload
    First submission

---
+++ P1HW1_Output_Bao-MaldonadoVanessa.py
@@ -0,0 +1,17 @@
+user_num = int(input('Enter integer:\n'))
+
+print('You entered:', user_num)
+
+num_squared = user_num * user_num
+num_cubed = user_num * user_num * user_num 
+
+print(user_num, 'squared is', num_squared)
+print('And', user_num, 'cubed is', num_cubed, '!!')
+
+user_num2 = int(input('Enter another integer:\n'))
+
+num_add = user_num + user_num2
+num_mult = user_num * user_num2
+
+print(user_num, '+', user_num2, 'is', num_add)
+print(user_num, '*', user_num2, 'is', num_mult)
